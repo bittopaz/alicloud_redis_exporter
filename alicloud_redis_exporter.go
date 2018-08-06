c2e8071c8f3500258f0956a6415e49df6c518829
haokang.ke <haokang.ke@owitho.com>
2018-08-06 12:11:39 +0800

    update

--- alicloud_redis_exporter.go
+++ alicloud_redis_exporter.go
@@ -64,7 +64,7 @@ func GetValue(InstanceId string, metric string) float64 {
 	request.Project = "acs_kvstore"
 	request.Dimensions = fmt.Sprintf("{\"instanceId\":\"%s\"}", InstanceId)
 	request.Metric = metric
-	request.Domain = "cms.cn-shanghai.aliyuncs.com"
+	request.Domain = "metrics.cn-shanghai.aliyuncs.com"
 	response, err := client.QueryMetricLast(request)
 	HandleErr(err)
 	var re aliResponse
